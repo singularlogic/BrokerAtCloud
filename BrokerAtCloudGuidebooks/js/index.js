ba7a6bdabaae44397d393cee97f252839a1931b1
Wolfgang Schwach <toraxx@gmail.com>
2015-07-03 13:54:06 +0200

    Added Broker@Cloud Guidebooks

---
+++ BrokerAtCloudGuidebooks/js/index.js
@@ -0,0 +1,45 @@
+$('#nav').affix({
+    offset: {     
+        top: $('#nav').offset().top,
+        bottom: ($('footer').outerHeight(true) + $('.application').outerHeight(true)) + 40
+    }
+});
+$(document).delegate('*[data-toggle="lightbox"]:not([data-gallery="navigateTo"])', 'click', function(event) {
+    event.preventDefault();
+    return $(this).ekkoLightbox({
+        onShown: function() {
+            if (window.console) {
+                return console.log('Checking our the events huh?');
+            }
+        },
+        onNavigate: function(direction, itemIndex) {
+            if (window.console) {
+                return console.log('Navigating '+direction+'. Current item: '+itemIndex);
+            }
+        }
+    });
+});
+//Programatically call
+$('#open-image').click(function (e) {
+    e.preventDefault();
+    $(this).ekkoLightbox();
+});
+$('#open-youtube').click(function (e) {
+    e.preventDefault();
+    $(this).ekkoLightbox();
+});
+// navigateTo
+$(document).delegate('*[data-gallery="navigateTo"]', 'click', function(event) {
+    event.preventDefault();
+    return $(this).ekkoLightbox({
+        onShown: function() {
+            var a = this.modal_content.find('.modal-footer a');
+            if(a.length > 0) {
+                a.click(function(e) {
+                    e.preventDefault();
+                    this.navigateTo(2);
+                }.bind(this));
+            }
+        }
+    });
+});
